3cf08477d3ebbcbf21c74897a0d83422c93db826
Minh Bui <ultraminh2709@gmail.com>
2022-11-25 21:03:05 -0800

    added telescope and startify as well as keybinds

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -41,74 +41,84 @@ packer.init({
 -- Install your plugins here
 return packer.startup(function(use)
 	-- My plugins here
-	use "wbthomason/packer.nvim" -- Have packer manage itself
-	use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-	use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+	use("wbthomason/packer.nvim") -- Have packer manage itself
+	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
 
 	-- Colorschemes
-	use "flazz/vim-colorschemes" -- Colorscheme for vim
+	use("flazz/vim-colorschemes") -- Colorscheme for vim
 
 	-- cmp plugins
-	use "hrsh7th/nvim-cmp" -- The completion plugin
-	use "hrsh7th/cmp-buffer" -- buffer completions
-	use "hrsh7th/cmp-path" -- path completions
-	use "hrsh7th/cmp-cmdline" -- cmdline completions
-	use "saadparwaiz1/cmp_luasnip" -- snippet completions
-	use "hrsh7th/cmp-nvim-lsp"
-	use "hrsh7th/cmp-nvim-lua"
+	use("hrsh7th/nvim-cmp") -- The completion plugin
+	use("hrsh7th/cmp-buffer") -- buffer completions
+	use("hrsh7th/cmp-path") -- path completions
+	use("hrsh7th/cmp-cmdline") -- cmdline completions
+	use("saadparwaiz1/cmp_luasnip") -- snippet completions
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lua")
 
 	-- snippets
-	use "L3MON4D3/LuaSnip" --snippet engine
-	use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+	use("L3MON4D3/LuaSnip") --snippet engine
+	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
 	-- LSP
-	use "neovim/nvim-lspconfig" -- enable LSP
-	use "williamboman/mason.nvim" -- simple to use language server installer
-	use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
-	use "jose-elias-alvarez/null-ls.nvim"
+	use("neovim/nvim-lspconfig") -- enable LSP
+	use("williamboman/mason.nvim") -- simple to use language server installer
+	use("williamboman/mason-lspconfig.nvim") -- simple to use language server installer
+	use("jose-elias-alvarez/null-ls.nvim")
 
 	-- Treesitter
-	use {
+	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
-	}
-	use "p00f/nvim-ts-rainbow"
+	})
+	use("p00f/nvim-ts-rainbow")
 
 	-- Commentary
-	use {
-		'numToStr/Comment.nvim',
+	use({
+		"numToStr/Comment.nvim",
 		config = function()
-			require('Comment').setup()
-		end
-	}
-	use 'JoosepAlviste/nvim-ts-context-commentstring'
+			require("Comment").setup()
+		end,
+	})
+	use("JoosepAlviste/nvim-ts-context-commentstring")
 
 	-- Nvim tree
-	use {
-		'nvim-tree/nvim-tree.lua',
+	use({
+		"nvim-tree/nvim-tree.lua",
 		requires = {
-			'nvim-tree/nvim-web-devicons', -- optional, for file icons
+			"nvim-tree/nvim-web-devicons", -- optional, for file icons
 		},
-		tag = 'nightly' -- optional, updated every week. (see issue #1193)
-	}
+		tag = "nightly", -- optional, updated every week. (see issue #1193)
+	})
 
 	-- lualine (the statusline on the bottom)
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
 	-- bufferline
-	use { 'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons' }
+	use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons" })
 
 	-- Make nvim background transparent
-	use "xiyaowong/nvim-transparent"
+	use("xiyaowong/nvim-transparent")
 
 	-- Toggleterm
-	use { "akinsho/toggleterm.nvim", tag = '*', config = function()
-		require("toggleterm").setup()
-	end }
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "*",
+	})
 
+	-- Telescope fuzzyfinder
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+
+	-- startify
+	use("mhinz/vim-startify")
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
---
+++ lua/user/startify.lua
@@ -0,0 +1,32 @@
+vim.g.startify_session_autoload = 1
+-- let startify take care of buffers
+vim.g.startify_session_delete_buffers = 1
+-- use unicode 
+vim.g.startify_fortune_use_unicode = 1
+-- auto update sessions
+vim.g.startify_session_persistence = 1
+-- delete empty buffer upon quit
+vim.g.startify_enable_special = 0
+-- custom header
+vim.g.startify_lists = {
+	{ type = 'files',     header = {'   Files'}            },
+	{ type = 'dir',       header =  {'   Current Directory '.. vim.fn.getcwd()} },
+	{ type = 'sessions',  header = {'   Sessions'}      },
+	{ type =  'bookmarks', header = {'   Bookmarks'}      },
+}
+vim.cmd([[
+let g:startify_bookmarks = [
+            \ { 'a': '~/src/astro-gauchoracing' },
+            \ { 'b': '~/.bashrc' },
+            \ { 'c': '~/.config/' },
+            \ { 'i': '~/.config/nvim/init.lua' },
+            \ { 'l': '~/.config/nvim/lua/user/' }
+            \ ]
+let g:startify_custom_header = [
+		\ ' __  __     _             _        (")                     _  _                              _ ',           
+		\ '|  \/  |   (_)    _ _    | |_       \|     ___      o O O | \| |    ___     ___    __ __    (_)    _ __ ',   
+		\ '| |\/| |   | |   | '' \   | | \           (_-<     o       | .` |   / -_)   / _ \   \ V /    | |   | ''  \ ',  
+		\ '|_|__|_|  _|_|_  |_||_|  |_||_|   _____   /__/_   TS__[O] |_|\_|   \___|   \___/   _\_/_   _|_|_  |_|_|_| ',
+		\ '_|"""""|_|"""""|_|"""""|_|"""""|_|     |_|"""""| {======|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""| ',
+		\ '"`-0-0-`"`-0-0-`"`-0-0-`"`-0-0-`"`-0-0-`"`-0-0-`./o--000`"`-0-0-`"`-0-0-`"`-0-0-`"`-0-0-`"`-0-0-`"`-0-0- ']
+]])
